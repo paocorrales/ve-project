e49fe41e68c8da33177c202897be9cb82ae4d06d
Pao Corrales <paobcorrales@gmail.com>
2024-08-26 17:45:21 +1000

    Expand help functions for percentiles

--- scripts/help_functions.R
+++ scripts/help_functions.R
@@ -16,19 +16,26 @@ group_eruptions <- function(time, threshold = 5) {
 # temperature_serie dt with time and t2m_a (temperature anomaly)
 # ini_dates eruption date vector
 # period lengh of the period to use to calculate the base temperature
+# percentiles logical t base for percentiles?
+# p char, which percentile if percentiles = TRUE
 #
 # Returns: a vector of lengh ini_dates
 
-calculate_tbase <- function(temperature_serie, ini_date, period = 12) {
-
+calculate_tbase <- function(temperature_serie, ini_date, period = 12, percentiles = FALSE, p = "p1") {
   
-  # purrr::map_dbl(ini_dates, function(ini_date) {
+  if(percentiles) {
     
     start_period <- lubridate::as_datetime(ini_date) - months(period)
     
-    as.numeric(temperature_serie[time %between% c(start_period, ini_date), 
-                                 .(base_t = mean(t2m_a))])
-  # }) 
+    return(as.numeric(temperature_serie[time %between% c(start_period, ini_date) & percentile == p, 
+                                        .(base_t = mean(t2m_a))]))
+  } else {
+    
+    start_period <- lubridate::as_datetime(ini_date) - months(period)
+    
+    return(as.numeric(temperature_serie[time %between% c(start_period, ini_date), 
+                                        .(base_t = mean(t2m_a))]))
+  }
   
 }
 
@@ -38,14 +45,23 @@ calculate_tbase <- function(temperature_serie, ini_date, period = 12) {
 # ini_date date of the eruption
 # before years before eruption
 # after years after eruption
+# percentiles logical t base for percentiles?
+# p char, which percentile if percentiles = TRUE
 # 
 # Returns: a list with time and temperature anomaly, compatible with data.table
 
-get_series <- function(t2_mean, ini_date, before = 1, after = 3) {
+get_series <- function(t2_mean, ini_date, before = 1, after = 3, percentiles = FALSE, p = "p1") {
   
   start_time <- ini_date - years(before)
   end_time <- ini_date + years(after)
-  temp <- t2_mean[time %between% c(start_time, end_time)] 
+  
+  if (percentiles) {
+    temp <- t2_mean[time %between% c(start_time, end_time) & percentile == p] 
+    
+  } else {
+    
+    temp <- t2_mean[time %between% c(start_time, end_time)]   
+  }
   
   list(time = temp$time,
        t2m_a = temp$t2m_a)
