2feb69a2c135d363fcdf9bb42e99e6e11bdcf1d5
Pao Corrales <paobcorrales@gmail.com>
2024-10-17 13:28:24 +1100

    generalise composite for volcanos

--- scripts/mean_composite_volcanos.R
+++ scripts/mean_composite_volcanos.R
@@ -6,39 +6,63 @@ library(unglue)
 
 strong <- readr::read_rds("~/ve-project/data/strong_volcanos.rds")
 
+var <- "2t"
 center_day <- 180
-half_window <- 30 # 30 is 2 months
+half_window <- 60 # 30 is 2 months
+period <- "after_ve" # base / after_ve
 
 
 for (v in strong$volcano) {
   
   message(v)
-  d <- strong[volcano == v]$ini_date + days(center_day)
+  if (var == "2t") {
+    var2 <- "t2m"
+  } else {
+    var2 <- var
+  }
   
-  times <- seq.Date(d - days(half_window), d + days(half_window), by = "day")
-  # times <- seq.Date(d - days(365), d - days(1), by = "days")
-  
+  if (period == "base") {
+    d <- strong[volcano == v]$ini_date
+    times <- list(seq.Date(d - days(365), d + days(0), by = "day"))
+  } else {
+    d <- strong[volcano == v]$ini_date + days(center_day)
+    times <- seq.Date(d - days(half_window), d + days(half_window), by = "day")
+  }
   
   for (t in times) {
     
     message(t)
+    file <- paste0("/g/data/w40/pc2687/ve/data_noenso/anomaly/", var, "_", formatC(month(as_date(t)), width = 2, flag = "0"), "_anomaly_mei.nc")
+    
     if (t == times[1]) {
       
-      temp <- ReadNetCDF("/scratch/w40/pc2687/tp_daily_deseasoned.nc", vars = "tp", subset = list(time = as_date(t)))
-      
-      temp[, time := NULL]
+      field <- ReadNetCDF(file, vars = var2, subset = list(time = as_date(t))) |> 
+        setnames(var2, "value") |> 
+        _[, .(latitude, longitude, value)]
       
     } else {
       
-      field <- rbind(temp, 
-                     ReadNetCDF("/scratch/w40/pc2687/tp_daily_deseasoned.nc", vars = "tp", subset = list(time = as_date(t))) |> 
-                       _[, time := NULL])  |> 
-        _[, .(tp = sum(tp)), by = .(longitude, latitude)]
+      field <- rbind(field,
+                     ReadNetCDF(file, vars = var2, subset = list(time = as_date(t))) |>
+                       setnames(var2, "value") |>
+                       _[, .(latitude, longitude, value)])  |>
+        _[, .(value = sum(value)), by = .(longitude, latitude)]
     }
     
   }
-  field[, let(tp = tp/length(times),
-              volcano = v)] |> 
-    readr::write_rds(x = _, paste0("~/ve-project/data2/tp_a", center_day/30, "m_", half_window*2/30, "m_composite_", v, ".rds"))
+  
+  if (period == "base") {
+    field[, let(value = value/nrow(times),
+                volcano = v)] |> 
+      setnames("value", var2) |> 
+      readr::write_rds(x = _, paste0("~/ve-project/data_noenso/derived/", var, "_base_composite_", v, ".rds"))
+  } else {
+    
+    field[, let(value = value/nrow(times),
+                volcano = v)] |> 
+      setnames("value", var2) |> 
+      readr::write_rds(x = _, paste0("~/ve-project/data_noenso/derived/", var, "_a", center_day/30, "m_", half_window*2/30, "m_composite_", v, ".rds"))
+  }
+  
 }
 
